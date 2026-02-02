5be211d4cc50efd62c6cebb77e57935881328c69
Noah P <noahpersaud@zohomail.com>
2026-02-02 17:14:29 -0500

    Add names.py

---
+++ names.py
@@ -0,0 +1,7 @@
+# would eventually be read from file. split by gender
+
+NAMES = ["Sam", "Alex", "Mia", "Leo", "Ava", "Noah", "Olivia"]
+
+def get_name(gender="male"):
+    # hard code for now
+    name = NAMES[-1]
