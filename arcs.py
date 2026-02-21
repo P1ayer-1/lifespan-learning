a91cf132f8276fee5a518d0494f44c9fe9d35030
Noah P <noahpersaud@zohomail.com>
2026-02-21 14:39:23 -0500

    Define ARCS for learning music and sports with conditioning and skill learning arcs

--- arcs.py
+++ arcs.py
@@ -1,10 +1,23 @@
 # arcs.py
 
 
-CONDITIONING_ARCS = {
 
-}
 
-SKILL_LEARNING_ARCS = {
-    
+ARCS = {
+    "learning_music": {
+        "conditioning_arc": {
+            "min_tier": 0,
+            "max_tier": 3,
+        },
+        "skill_learning_arc": {},
+        "description": "learning music",
+    },
+    "learning_sports": {
+        "conditioning_arc": {
+            "min_tier": 0,
+            "max_tier": 3,
+        },
+        "skill_learning_arc": {},
+        "description": "learning sports",
+    },
 }
