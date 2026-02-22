0ce20437bd8d5527c489ba995ed8c779fedda114
Noah P <noahpersaud@zohomail.com>
2026-02-21 20:20:16 -0500

    Add shared locations for learning music, sports, and religious or cultural practices

--- arcs.py
+++ arcs.py
@@ -11,6 +11,10 @@ ARCS = {
         },
         "skill_learning_arc": {},
         "description": "learning music",
+        "shared_locations": [
+            "at home", "at school", "at grandparent's house", "at a friend's house", "at a community center",
+            "at a music studio", "at a concert hall", "at a music store", "at a band practice room",
+        ]
     },
     "learning_sports": {
         "conditioning_arc": {
@@ -19,5 +23,21 @@ ARCS = {
         },
         "skill_learning_arc": {},
         "description": "learning sports",
+        "shared_locations": [
+            "at home", "at school", "at grandparent's house", "at a friend's house", "at a community center",
+            "at a sports field", "at a gym", "at a sports club", "during a sports practice",
+        ]
+    },
+    "learning_religious_or_cultural_practices": {
+        "conditioning_arc": {
+            "min_tier": 0,
+            "max_tier": 3,
+        },
+        "skill_learning_arc": {},
+        "description": "learning religious or cultural practices",
+        "shared_locations": [
+            "at home", "at school", "at grandparent's house", "at a friend's house", "at a community center",
+            "at a religious or cultural center", "during a religious or cultural ceremony",
+        ]
     },
 }
