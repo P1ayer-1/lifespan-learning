588bd50a20c698c4f3852c7db7bf7ce1069ec9b3
Noah Persaud <88407894+P1ayer-1@users.noreply.github.com>
2026-02-28 17:44:30 -0500

    Refactor Arc class: inherit from ContentType and streamline initialization

--- src/lifespan_learning/dataset_generation/prompt/engine/arcs.py
+++ src/lifespan_learning/dataset_generation/prompt/engine/arcs.py
@@ -1,26 +1,17 @@
 # arcs.py
 from .generation_configs import PromptConfig, ContentTypeConfig
+from .content_type import ContentType
 
 
-class Arc:
-    def __init__(self, config: dict):
-        pass
+class Arc(ContentType):
+    def __init__(self, config: ContentTypeConfig):
+        super().__init__(config)
 
 
 class BasicLearningArc(Arc):
     def __init__(self, config: ContentTypeConfig):
         super().__init__(config)
-        self.key = config.key
-        self.description = config.description
-        self.locations = config.locations
-        self.min_tier = config.min_tier
-        self.max_tier = config.max_tier
 
 class AdvancedLearningArc(Arc):
     def __init__(self, config: ContentTypeConfig):
         super().__init__(config)
-        self.key = config.key
-        self.description = config.description
-        self.locations = config.locations
-        self.min_tier = config.min_tier
-        self.max_tier = config.max_tier
